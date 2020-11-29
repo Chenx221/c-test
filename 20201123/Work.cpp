222d67696e519fc5f7d1c070d3ec05743becd987
Chenx221 <chenx221@yandex.com>
2020-11-29 21:18:56 +0800

    Update Work.cpp

--- 20201123/Work.cpp
+++ 20201123/Work.cpp
@@ -1,23 +1,36 @@
 #include<stdio.h>
+void reset(); 
+int n[5];
 int main() {
-    int pn,score[999],temp,i,n[7];
-    /*
-    n[] 1a 2b 3c 4d 5e
-    */
+    int pn,i;reset();
     printf("学生数量：");
-    scanf("%d",&pn);
+    scanf("%d",&pn);int score[pn];
     printf("键入成绩: \n");
     for (i =1;i<=pn;i++) {
         scanf("%d",&score[i]);
-        getchar();
-        if (score[i]>=90) n[1]++;
+        if (score[i]>100) {
+			i--;
+			printf("请输入0~100之间的分数\n");
+			continue;
+		}
+        else if (score[i]>=90) n[1]++;
         else if (score[i]>=80) n[2]++;
         else if (score[i]>=70) n[3]++;
         else if (score[i]>=60) n[4]++;
         else n[5]++;
-        }
-    printf("********************分割线********************\n");
-    printf("A: %d\nB: %d\nC: %d\nD: %d\nE: %d\n",n[1],n[2],n[3],n[4],n[5]);
-
-    return 0;
     }
+        
+    printf("********************分割线********************\n");
+    for (int i=65,k=1;k<=5;i++,k++) {
+		printf("%c：%d\n",i,n[k]);
+	}
+	
+    return 0;
+}
+    
+void reset(void) {
+	int i=1;
+	while (i<=5) {
+		n[i]=0;i++;
+	}
+}
